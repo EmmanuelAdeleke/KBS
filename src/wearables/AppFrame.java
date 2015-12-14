044be12d26e01e4874f80503c19d14366174d3e5
Emmanuel Adeleke <emi_emi@hotmail.co.uk>
2015-12-14 00:59:26 +0000

    Minor refactor and comments

--- src/wearables/AppFrame.java
+++ src/wearables/AppFrame.java
@@ -1,31 +1,21 @@
 package wearables;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Image;
 import java.awt.Insets;
-import java.awt.TextArea;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.AbstractAction;
-import javax.swing.Action;
 import javax.swing.ImageIcon;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
-import javax.swing.JTextPane;
-
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.swing.SwingConstants;
 
 public class AppFrame extends JFrame {
@@ -41,6 +31,7 @@ public class AppFrame extends JFrame {
 			public void run() {
 				try {
 					AppFrame frame = new AppFrame();
+					// Set visible to true and resizable to false
 					frame.setVisible(true);
 					frame.setResizable(false);
 				} catch (Exception e) {
@@ -54,16 +45,21 @@ public class AppFrame extends JFrame {
 	 * Create the frame.
 	 */
 	public AppFrame() {
+		
+		// Instantiate reasoner object
 		final Reasoner reasoner = new Reasoner("Wearables");
 
 		try {
+			// Initialize variables (load objects from XML file into appropriate classes)
 			reasoner.init();
 		} catch (FileNotFoundException e1) {
 			e1.printStackTrace();
 		}
 
+		// Create str builder object
 		final StringBuilder chatLog = new StringBuilder();
 
+		// Configure JFrame window //
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1000, 800);
 		contentPane = new JPanel();
@@ -72,12 +68,6 @@ public class AppFrame extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		JLabel lblEnter = new JLabel("Enter question");
-		lblEnter.setForeground(Color.WHITE);
-		lblEnter.setFont(new Font("Lantinghei TC", Font.PLAIN, 14));
-		lblEnter.setBounds(270, 684, 100, 16);
-		contentPane.add(lblEnter);
-
 		Image imgQuestion = new ImageIcon(this.getClass().getResource("/wearable.png")).getImage();
 		JLabel lblQuestion = new JLabel("");
 		lblQuestion.setIcon(new ImageIcon(imgQuestion));
@@ -85,9 +75,9 @@ public class AppFrame extends JFrame {
 		getContentPane().add(lblQuestion);
 
 		JLabel lblNewLabel = new JLabel("WearablesDirect");
-		lblNewLabel.setFont(new Font("Lantinghei TC", Font.PLAIN, 25));
+		lblNewLabel.setFont(new Font("Lantinghei TC", Font.PLAIN, 42));
 		lblNewLabel.setForeground(Color.WHITE);
-		lblNewLabel.setBounds(82, 44, 210, 34);
+		lblNewLabel.setBounds(116, 59, 409, 34);
 		contentPane.add(lblNewLabel);
 
 		JLabel lblC = new JLabel("Welcome to WearablesDirect");
@@ -120,25 +110,44 @@ public class AppFrame extends JFrame {
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(33, 146, 594, 511);
 		getContentPane().add(scrollPane);
-
+		
+		JLabel lblEnter = new JLabel("Enter question");
+		lblEnter.setForeground(Color.WHITE);
+		lblEnter.setFont(new Font("Lantinghei TC", Font.PLAIN, 14));
+		lblEnter.setBounds(270, 684, 100, 16);
+		contentPane.add(lblEnter);
+		
 		final JTextArea textArea = new JTextArea();
 		scrollPane.setViewportView(textArea);
 		textArea.setEditable(false);
 		textArea.setMargin(new Insets(10,10,10,10));
-
+		
 		textField = new JTextField();
 		textField.setBounds(270, 712, 489, 34);
 		contentPane.add(textField);
 		textField.setColumns(10);
-
+		
+		// Set action listener (onEnter) for textField
 		textField.addActionListener(new ActionListener() {
+			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				String question = textField.getText();
-				textArea.setText(question);
-				chatLog.append("> " + question + "\n");
-				chatLog.append(reasoner.generateAnswer(question) + "\n");
+				
+				// If textField is empty...
+				if (textField.getText().trim().isEmpty())
+					chatLog.append("Don't leave me empty... type something!\n");
+				else {
+					// Get text from textField and store in question
+					String question = textField.getText();
+					// Append question to the chat log
+					chatLog.append("> " + question + "\n");
+					// Append the answer to the chat log
+					chatLog.append(reasoner.generateAnswer(question) + "\n");
+				}
+				
+				// Reset the text field
 				textField.setText("");
+				// Display chat log to the text area
 				textArea.setText(chatLog.toString() + "\n");
 			}
 		});
